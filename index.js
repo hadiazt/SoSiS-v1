3f3dc923f5faad0a1e5566806aff708014963e6b
hadi <65118892+hadiazt@users.noreply.github.com>
2021-08-21 04:47:45 +0000

    added some messages

--- index.js
+++ index.js
@@ -543,23 +543,23 @@ client.on("message", async message => {
       stream = YTDL(args.join(" "), { encoderArgs: ['-af', 'dynaudnorm=f=200'], fmt: 'mp3', opusEncoded: false });
       infos = await ScrapeYt.search(args.join(" "));
     } catch (e) {
-      return message.inlineReply('<a:cross:853953928269660180> | چیزی برای لینک وارد شده یافت نشد\n' + '```js\n' + e + '\n```')
+      return message.inlineReply('<a:cross:853953928269660180> | چیزی برای لینک وارد شده یافت نشد')
     }
 
     try {
-      message.inlineReply('<a:load:878160361302401034> | پس از اتمام دانلود فایل مورد نظر ارسال میشود');
+      message.inlineReply('<a:load:878160361302401034> | پس از اتمام دانلود فایل مورد نظر ارسال میشود\nدر نظر داشته باشید فایل موردنظر زیر 8 مگ باشد');
 
       stream.pipe(createWriteStream(__dirname + `/download/${infos[0].id}.mp3`)).on('finish', () => {
 
         try {
           message.inlineReply(`<a:tick:853953922426470400> | آهنگ درخواستی شما ${infos[0].title} `, new Discord.MessageAttachment(__dirname + `/download/${infos[0].id}.mp3`, `${infos[0].id}.mp3`))
         } catch (e) {
-          return message.inlineReply('<:erorr:878139495764090880> | به دو دلیل ارسال فایل امکان پذیر نیست\n1) حجم فایل بیش از 8 مگابایت است \n2) ربات دسترسی ارسال محتوا را در چنل/سرور ندارد\n' + '```js\n' + e + '\n```');
+          return message.inlineReply('<:erorr:878139495764090880> | به دو دلیل ارسال فایل امکان پذیر نیست\n1) حجم فایل بیش از 8 مگابایت است \n2) ربات دسترسی ارسال محتوا را در چنل/سرور ندارد');
         }
 
       })
     } catch (e) {
-      return message.inlineReply('<a:cross:853953928269660180> | چیزی برای لینک وارد شده یافت نشد\n' + '```js\n' + e + '\n```')
+      return message.inlineReply('<a:cross:853953928269660180> | چیزی برای لینک وارد شده یافت نشد')
     }
     client.channels.cache.get(config.ACTION_LOG).send('```\n' + 'yt dn triggerd in ' + message.guild.name + ' server | by ' + message.author.username + ' | in ' + message.channel.name + '\n```');
   }
