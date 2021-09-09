746e5e5bf07f7707a3cc5b0d4085e8c186cb7964
hadi <65118892+hadiazt@users.noreply.github.com>
2021-09-09 15:22:18 +0000

    added express

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// =============== Port ===============
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => res.send('Bot Is Working Well!'));
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+// =============== Bot ===============
+
+
 const Discord = require('discord.js');
 require("./ExtendedMessage");
 const client = new Discord.Client({ allowedMentions: { repliedUser: true } });
